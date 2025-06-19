6e7ca1d4f4d9c090f178e4b5e48e1fef3ee7ce98
Cédric CEPEDA <cedriic.cepeda@gmail.com>
2025-06-19 16:18:36 +0200

    V1.0.29

--- electron/main.ts
+++ electron/main.ts
@@ -63,7 +63,6 @@ function createWindow() {
   if (VITE_DEV_SERVER_URL) {
     win.loadURL(VITE_DEV_SERVER_URL)
   } else {
-    // win.loadFile('dist/index.html')
     win.loadFile(path.join(RENDERER_DIST, 'index.html'))
   }
 
